366e0b8d16de7877e56c755e9900413108ed204b
Camila <85712574004@senacrs.edu.br>
2025-09-01 10:15:51 -0300

    alteracao

---
+++ include/conexao.php
@@ -0,0 +1,11 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBBASE', 'empresa');
+
+$conexao = mysqli_connect(DBHOST, DBUSER, DBPASS, DBBASE);
+
+
+
+
